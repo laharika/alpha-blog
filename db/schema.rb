005e4e04ecbb6659e87eaead34d759dfecda5439
Laharik <laharika9ty4@gmail.com>
2018-12-18 14:08:33 +0530

    article creation and display

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_114246) do
+ActiveRecord::Schema.define(version: 2018_12_14_115131) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
